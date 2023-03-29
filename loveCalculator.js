19bd464babc157dc3a2f2f8eb1b367acbfa00275
Zach Ruiz <powersbooth10@gmail.com>
2023-03-29 12:00:08 -0500

    changed the document from .html to .js and added some conditional logic.

--- loveCalculator.js
+++ loveCalculator.js
@@ -1,7 +1,7 @@
 prompt("What is your name?");
 prompt("What is their name?");
 
-<!-- //random number between 1 and 100 -->
+//random number between 1 and 100
 let loveScore = Math.random() * 100;
 loveScore = Math.floor(loveScore) + 1;
 console.log(loveScore);
@@ -9,5 +9,5 @@ console.log(loveScore);
 if (loveScore === 100) {
     alert("Your love is " + loveScore + "%. " + "You love each other like a fat kid loves cake!")
 } else {
-    alert("Your love is " + loveScore + "%. + "You love each other like a fat kid loves cake!""
+    alert("Your love is " + loveScore + "%." + "You love each other like a fat kid loves cake!")
 }
